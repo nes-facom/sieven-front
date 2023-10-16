6e379287bb27dd664040930d0901f2da6bfb1ce5
Arthur Cabral da Silva <artcdsilva@fornecedor.qualicorp.com.br>
2023-10-16 17:52:18 -0400

    Alteração card text

--- src/pages/paginaInicial/index.vue
+++ src/pages/paginaInicial/index.vue
@@ -1,23 +1,30 @@
-
 <template>
-  <v-sheet class="mx-auto" max-width=100vh>
-    <v-slide-group show-arrows>
-      <v-slide-group-item v-for="n in 10" :key="n">
+  <v-sheet class="mx-auto">
+    <v-carousel
+      style="
+        max-height: 548px;
+        max-width: 1922px;
+        width: 100%;
+        height: 100%;
+        background-color: #fafafa;
+      "
+    >
+      <v-carousel-item v-for="event in events" :key="event.id">
         <v-row>
-          <v-col cols="6">
+          <v-col cols="12" md="6">
             <v-img
               class="justify-end text-white"
-              height="300"
-              width="50vh"
-              src="https://cbsoft2023.ufms.br/images/logos/cbsoft2023-horizontal-menor.png"
-              cover
+              height="548"
+              width="781"
+              :src="event.image"
+              contain
             ></v-img>
           </v-col>
-          <v-col cols="6">
-            <v-card class="mx-auto" max-width="50vh" height="300">
-              <v-card-subtitle class="pt-3"> Congresso Brasileiro de Software</v-card-subtitle> <!-- pegar infos do backend -->
+          <v-col cols="12" md="6">
+            <v-card class="mx-auto" max-width="50vh" height="75%">
+              <v-card-subtitle class="pt-3">{{ event.title }}</v-card-subtitle>
               <v-card-text>
-                <div>O Congresso Brasileiro de Software: Teoria e Prática (CBSoft) é um evento realizado anualmente pela Sociedade Brasileira de Computação (SBC) com o objetivo de promover e incentivar a troca de experiências entre pesquisadores e profissionais da indústria e academia sobre as mais recentes pesquisas, tendências e inovações práticas e teóricas sobre software</div>
+                <div>{{ event.description }}</div>
               </v-card-text>
               <v-card-actions>
                 <v-btn color="blue" depressed elevation="6"> SAIBA MAIS </v-btn>
@@ -25,54 +32,33 @@
             </v-card>
           </v-col>
         </v-row>
-      </v-slide-group-item>
-    </v-slide-group>
+      </v-carousel-item>
+    </v-carousel>
   </v-sheet>
 </template>
 
 <script>
-
 export default {
-  name: "pgPaginaInicialIndex",
+  name: "randomResponse",
   data() {
     return {
-      sieven: [
-        { id: 1, titulo: 'Ensino', icone: 'mdi-school-outline', tamanho: '50%'},
-        { id: 2, titulo: 'Pesquisa', icone: 'mdi-magnify', tamanho: '70%'},
-        { id: 3, titulo: 'Extensão', icone: 'mdi-book-arrow-right-outline', tamanho: '90%'}
-      ]
-    }
-  },
-  methods: {
-    redirecionarAgetic() {
-      window.open("https://agetic.ufms.br/", "_blank")
-    },
-    redirecionarUfms() {
-      window.open("https://www.ufms.br/", "_blank")
-    },
-    redirecionarFacom() {
-      window.open("https://www.facom.ufms.br/", "_blank")
-    },
-    redirecionarNes() {
-      window.open("https://nes.facom.ufms.br/projeto/sieven-sistema-de-gestao-de-eventos", "_blank")
-    }
+      events: [],
+    };
   },
   created() {
-    this.$store.dispatch('deslogar')
-  }
-}
+    this.fetchRandomData();
+  },
+  methods: {
+    fetchRandomData() {
+      for (let i = 1; i <= 5; i++) {
+        this.events.push({
+          id: i,
+          title: "Evento Aleatório " + i,
+          description: "Descrição aleatória do evento " + i,
+          image: `https://picsum.photos/1922/548?random=${i}`,
+        });
+      }
+    },
+  },
+};
 </script>
-
-<style scoped>
-html, body {
-  height: 100%;
-  margin: 0px;
-}
-#pgPaginaInicial {
-  height: 100vh;
-  width: 100vw;
-  background-image: url("../../assets/background-login.jpg");
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-}
-</style>
