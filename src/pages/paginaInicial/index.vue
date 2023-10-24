6ef74b0d8d8f7f24a7a8d43ce9c42a9bae951e69
Brunno <brunnofern064@gmail.com>
2023-10-24 19:32:23 -0400

    US1: Novo componente do carousel

--- src/pages/paginaInicial/index.vue
+++ src/pages/paginaInicial/index.vue
@@ -1,85 +1,52 @@
-
 <template>
-  <v-sheet>
-    <v-carousel
-      style="
-        max-height: 34.25em;
-        max-width: 120.125em;
-        background-color: #fafafa;
-      "
-    >
-      <v-carousel-item v-for="event in events" :key="event.id">
-        <v-row>
-          <v-col cols="6">
-            <v-img
-              class="justify-end text-white"
-              height="100%"
-              max-width="43em"
-              :src="event.image"
-              contain
-            ></v-img>
-          </v-col>
-          <v-col cols="6">
-            <!-- Card -->
-            <v-card max-width="43em" height="100%" position="relative" style="background-color: #fafafa">
-              <v-card-subtitle class="pt-3">{{ event.title }}</v-card-subtitle>
-              <v-card-text>
-                <div>{{ event.description }}</div>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn color="blue" depressed elevation="6"> SAIBA MAIS </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-carousel-item>
-    </v-carousel>
-    MAIS EVENTOS
-    <v-footer color="black" app>
-      <v-container>
-        <v-layout align-center justify-center>
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on, attrs }">
-              <img
-                src="@/assets/home/nes.png"
-                alt="NES - Núcleo de Práticas de Software"
-                style="width: 39px; height: 45px;"
-                v-bind="attrs"
-                v-on="on"
-              />
-            </template>
-            <span>Texto flutuante</span>
-          </v-tooltip>
-          <img src="@/assets/home/nes.png" alt="Imagem Aleatória" style="width: 39px; height: 45px;" />
-        </v-layout>
-        NES
-      </v-container>
-    </v-footer>
-  </v-sheet>
+  <v-carousel>
+    <v-carousel-item v-for="(event, index) in events" :key="index">
+      <v-row align="center" justify="center" class="fill-height">
+        <!-- Imagem do evento -->
+        <v-col cols="12" md="4" class="text-center">
+          <v-img :src="event.image" class="rounded-lg" max-height="300"></v-img>
+        </v-col>
+        
+        <!-- Detalhes do evento -->
+        <v-col cols="12" md="6" class="text-center">
+          <h2 class="headline">{{ event.title }}</h2>
+          <p>{{ event.description }}</p>
+          <p class="caption">{{ event.date }} | {{ event.time }}</p>
+          <v-btn @click="showDetails(event)" color="primary">Saiba Mais</v-btn>
+        </v-col>
+      </v-row>
+    </v-carousel-item>
+  </v-carousel>
 </template>
-
 
 <script>
 export default {
-  name: "randomResponse",
+  name: "paginaInicial",
   data() {
     return {
-      events: [],
+      events: [
+        {
+          title: 'Evento 1',
+          description: 'Descrição do Evento 1.',
+          date: '01/01/2023',
+          time: '14:00',
+          image: "../../assets/agetic_logo.png",
+        },
+        {
+          title: 'Evento 2',
+          description: 'Descrição do Evento 2.',
+          date: '02/01/2023',
+          time: '15:30',
+          image: "../../assets/agetic_logo.png",
+        },
+        // Adicione mais eventos conforme necessário
+      ],
     };
   },
-  created() {
-    this.fetchRandomData();
-  },
   methods: {
-    fetchRandomData() {
-      for (let i = 1; i <= 5; i++) {
-        this.events.push({
-          id: i,
-          title: "Evento Aleatório " + i,
-          description: "Descrição aleatória do evento " + i,
-          image: `https://picsum.photos/1922/548?random=${i}`,
-        });
-      }
+    showDetails(event) {
+      // Lógica para exibir mais detalhes do evento
+      console.log('Detalhes do evento:', event);
     },
   },
 };
