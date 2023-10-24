20cea9732e2f5776e1ef5eac436fb94f1477a877
Arthur Cabral da Silva <ac-silva95@hotmail.com>
2023-10-23 22:36:54 -0400

    alteração carousel

--- src/pages/paginaInicial/index.vue
+++ src/pages/paginaInicial/index.vue
@@ -1,28 +1,27 @@
 
 <template>
-  <v-sheet class="mx-auto">
+  <v-sheet>
     <v-carousel
       style="
-        max-height: 548px;
-        max-width: 1922px;
-        width: 100%;
-        height: 100%;
+        max-height: 34.25em;
+        max-width: 120.125em;
         background-color: #fafafa;
       "
     >
       <v-carousel-item v-for="event in events" :key="event.id">
         <v-row>
-          <v-col cols="12" md="6">
+          <v-col cols="6">
             <v-img
               class="justify-end text-white"
-              height="548"
-              width="781"
+              height="100%"
+              max-width="43em"
               :src="event.image"
               contain
             ></v-img>
           </v-col>
-          <v-col cols="12" md="6">
-            <v-card class="mx-auto" max-width="50vh" height="75%">
+          <v-col cols="6">
+            <!-- Card -->
+            <v-card max-width="43em" height="100%" position="relative" style="background-color: #fafafa">
               <v-card-subtitle class="pt-3">{{ event.title }}</v-card-subtitle>
               <v-card-text>
                 <div>{{ event.description }}</div>
@@ -37,27 +36,28 @@
     </v-carousel>
     MAIS EVENTOS
     <v-footer color="black" app>
-    <v-container>
-      <v-layout align-center justify-center>
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on, attrs }">
-            <img
-              src="@/assets/home/nes.png"
-              alt="NES - Núcleo de Práticas de Software"
-              style="width: 39px; height: 45px;"
-              v-bind="attrs"
-              v-on="on"
-            />
-          </template>
-          <span>Texto flutuante</span>
-        </v-tooltip>
-        <img src="@/assets/home/nes.png" alt="Imagem Aleatória"  style="width: 39px; height: 45px;"/>
-      </v-layout>
-    </v-container>
-  </v-footer>
+      <v-container>
+        <v-layout align-center justify-center>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <img
+                src="@/assets/home/nes.png"
+                alt="NES - Núcleo de Práticas de Software"
+                style="width: 39px; height: 45px;"
+                v-bind="attrs"
+                v-on="on"
+              />
+            </template>
+            <span>Texto flutuante</span>
+          </v-tooltip>
+          <img src="@/assets/home/nes.png" alt="Imagem Aleatória" style="width: 39px; height: 45px;" />
+        </v-layout>
+        NES
+      </v-container>
+    </v-footer>
   </v-sheet>
-  
 </template>
+
 
 <script>
 export default {
