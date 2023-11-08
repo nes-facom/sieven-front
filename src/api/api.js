45e6993258d10d4deb8bc4f8dc928b7b58049ad1
Brunno <brunnofern064@gmail.com>
2023-11-07 20:28:52 -0400

    USX: Alterando link da api para review

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiSieven = axios.create({
-    baseURL: 'https://52.206.163.83/api'   ,
+    baseURL: 'https://127.0.0.1:8000/api'   ,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
