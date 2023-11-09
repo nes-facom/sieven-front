2769665439641cebcfb93f78125a73c58b2560ab
Brunno <brunnofern064@gmail.com>
2023-11-08 20:58:48 -0400

    USX: Aumentando o tempo de timeout da api

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiSieven = axios.create({
     baseURL: 'http://52.206.163.83/api'   ,
-    timeout: 10000,
+    timeout: 100000,
     headers: {
         'Content-Type': 'application/json'
     }
