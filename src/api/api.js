f9f354099dc8d26accbd97c683d32e6eda6e98b1
Larissa Leal <larissa.souza@startaideia.com.br>
2023-11-09 17:13:49 -0400

    USX: atualizacao do arquivo api.js usando https

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,11 @@
-import axios from 'axios'
+import axios from "axios";
 
 const apiSieven = axios.create({
-    baseURL: 'http://52.206.163.83/api'   ,
-    timeout: 100000,
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
+  baseURL: "https://52.206.163.83/api",
+  timeout: 100000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-export { apiSieven }
+export { apiSieven };
