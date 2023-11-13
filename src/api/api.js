0eb87a91829ef7ce4ddb2208797d75306b643c4d
Larissa Leal <larissa.souza@startaideia.com.br>
2023-11-13 16:29:38 -0400

    USX: teste api.js usando http

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiSieven = axios.create({
-  baseURL: "https://52.206.163.83/api",
+  baseURL: "http://52.206.163.83/api",
   timeout: 100000,
   headers: {
     "Content-Type": "application/json",
