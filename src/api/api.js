d0b71f19031e3ac9363436d112d84fd4fb7622ec
Larissa Leal <larissa.souza@startaideia.com.br>
2023-11-21 17:31:14 -0400

    USX: troca da url do backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiSieven = axios.create({
-  baseURL: "http://52.206.163.83/api",
+  baseURL: "https://sieven-backend.onrender.com/api",
   timeout: 100000,
   headers: {
     "Content-Type": "application/json",
