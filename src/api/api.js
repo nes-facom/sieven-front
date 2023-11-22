d2095e9a1157d1af0511e7df42a57ff0e3d0bc0f
BRUNNO FERNANDES LEWIN AUGUSTO <brunno.lewin@dominio.ufms.br>
2023-11-22 14:24:47 -0400

    US1: Alterando https para http

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiSieven = axios.create({
-  baseURL: "https://sieven-backend.onrender.com/api",
+  baseURL: "http://sieven-backend.onrender.com/api",
   timeout: 100000,
   headers: {
     "Content-Type": "application/json",
