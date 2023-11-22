d64c31fd491f210a1551d23a07fd570a3ae2191f
BRUNNO FERNANDES LEWIN AUGUSTO <brunno.lewin@dominio.ufms.br>
2023-11-22 14:45:59 -0400

    US1: voltando para https

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiSieven = axios.create({
-  baseURL: "http://sieven-backend.onrender.com/api",
+  baseURL: "https://sieven-backend.onrender.com/api",
   timeout: 100000,
   headers: {
     "Content-Type": "application/json",
